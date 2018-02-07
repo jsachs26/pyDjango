ab45dd501efa6f53b0bf408b7fe1f6ac23176192
jsachs26 <34320041+jsachs26@users.noreply.github.com>
2018-02-07 12:54:13 -0700

    Third TIME

--- pop2.py
+++ pop2.py
@@ -57,7 +57,7 @@ def populate():
         for p in Page.objects.filter(category=c):
             print("- {0} - {1}".format(str(c), str(p)))
 
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, viewd):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url=url
     p.views=views
@@ -65,7 +65,7 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name, views=0, likes=0):
+def add_cat(name, views, likes):
     c = Category.objects.get_or_create(name=name)[0]
     c.views=views
     c.likes=likes
